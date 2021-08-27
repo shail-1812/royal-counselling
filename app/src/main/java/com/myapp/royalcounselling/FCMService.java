93a640ef8e535539389aa59223494683492959c8
Jap Purohit <pjap041@gmail.com>
2021-08-27 12:10:59 +0530

    Logo

--- app/src/main/java/com/myapp/royalcounselling/FCMService.java
+++ app/src/main/java/com/myapp/royalcounselling/FCMService.java
@@ -1,16 +1,38 @@
 package com.myapp.royalcounselling;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class FCMService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    private static int count = 0;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
+        try {
+            sendNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("message"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -18,5 +40,42 @@ public class FCMService extends FirebaseMessagingService {
         super.onNewToken(s);
         getSharedPreferences("MYAPP", MODE_PRIVATE).edit().putString("TOKEN", s).apply();
 
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void sendNotification(String title, String messageBody) {
+        Intent intent = new Intent(getApplicationContext(), SplashScreenActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("pushnotification", "yes");
+        if(messageBody.contains("presentation")){
+            intent.putExtra("activityToDirect","PPTRequest");
+        }
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationManager mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel mChannel = new NotificationChannel("Sesame", "Sesame", importance);
+            mChannel.setDescription(messageBody);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+
+            mNotifyManager.createNotificationChannel(mChannel);
+        }
+       NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "Seasame");
+        mBuilder.setContentTitle(title)
+                .setContentText(messageBody)
+                .setSmallIcon(R.drawable.logo)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.logo))
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setColor(Color.parseColor("#FFD600"))
+                .setContentIntent(pendingIntent)
+                .setChannelId("Sesame")
+                .setPriority(NotificationCompat.PRIORITY_LOW);
+
+        mNotifyManager.notify(count, mBuilder.build());
+        count++;
     }
 }
