3d3f949296186c19e726c081db49126aa4e399f1
Jap Purohit <pjap041@gmail.com>
2021-09-07 17:37:05 +0530

    Bluh Bluh

--- app/src/main/java/com/myapp/royalcounselling/Utils.java
+++ app/src/main/java/com/myapp/royalcounselling/Utils.java
@@ -2,7 +2,7 @@ package com.myapp.royalcounselling;
 
 public class Utils {
 //    public static String main_url = "http://royalcareercounselling.co.in/";
-        public static String main_url = "http://148.72.245.28/royal_counselling_app-1/";
+        public static String main_url = "http://royalcareercounselling.co.in/";
 
 }
 
