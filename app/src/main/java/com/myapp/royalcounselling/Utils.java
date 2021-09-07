a15c85c4207944c1c3367a70132ecbac6d45c0a8
Shail Patel <shaileng181@gmail.com>
2021-09-07 21:36:29 +0530

    Final Changes 28

--- app/src/main/java/com/myapp/royalcounselling/Utils.java
+++ app/src/main/java/com/myapp/royalcounselling/Utils.java
@@ -2,7 +2,7 @@ package com.myapp.royalcounselling;
 
 public class Utils {
 //    public static String main_url = "http://royalcareercounselling.co.in/";
-        public static String main_url = "http://royalcareercounselling.co.in/";
+        public static String main_url = "https://royalcareercounselling.co.in/";
 
 }
 
